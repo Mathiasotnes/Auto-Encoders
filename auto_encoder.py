6c5b50eb0d7b48b25aceccbfe601ff1a73d2ba2d
Mathias Otnes <Mathias.otnes@gmail.com>
2024-04-09 19:55:01 +0200

    Support for color stacking in auto encoder

--- auto_encoder.py
+++ auto_encoder.py
@@ -8,11 +8,17 @@ import numpy as np
 
 class AutoEncoderNet:
     
-    def __init__(self, force_learn: bool = False, file_name: str = "./models/autoencoder/autoencoder", encoding_dim: np.int64 = 128, input_shape: tuple = (28, 28, 1)):
+    def __init__(self, mode: str = 'mono', force_learn: bool = False, file_name: str = "./models/autoencoder/autoencoder", encoding_dim: np.int64 = 128):
+        self.mode = mode
         self.force_relearn = force_learn
         self.file_name = file_name
         self.encoding_dim = encoding_dim
-        self.input_shape = input_shape
+        if self.mode == 'mono':
+            self.input_shape = (28, 28, 1)
+        elif self.mode == 'color':
+            self.input_shape = (28, 28, 3)
+        else:
+            raise ValueError("Mode must be 'mono' or 'color'")
         self.encoder, self.decoder, self.autoencoder = self._build_model()
 
     def _build_model(self):
@@ -52,15 +58,17 @@ class AutoEncoderNet:
         self.done_training = self.load_weights()
 
         if self.force_relearn or self.done_training is False:
-            # Get hold of data
-            x_train, y_train = generator.get_full_data_set(training=True)
-            x_test, y_test = generator.get_full_data_set(training=False)
+            # Get hold of data, we only need the images, not the labels
+            x_train, _ = generator.get_full_data_set(training=True)
+            x_test, _ = generator.get_full_data_set(training=False)
 
-            # "Translate": Only look at "red" channel; only use the last digit. Use one-hot for labels during training
-            x_train = x_train[:, :, :, [0]]
-            y_train = keras.utils.to_categorical((y_train % 10).astype(np.int64), 10)
-            x_test = x_test[:, :, :, [0]]
-            y_test = keras.utils.to_categorical((y_test % 10).astype(np.int64), 10)
+            if self.mode == 'mono':
+                # Process data for mono mode using only the first channel "red"
+                x_train = x_train[:, :, :, [0]]
+            
+            elif self.mode == 'color':
+                # Process data for color mode using all three channels
+                pass
 
             # Fit model
             self.autoencoder.fit(x=x_train, y=x_train, batch_size=1024, epochs=epochs,
@@ -98,23 +106,31 @@ class AutoEncoderNet:
         Compare original images with their reconstructions
         """
         x_test, _ = generator.get_random_batch(training=False, batch_size=num_images)
-        x_test = x_test[:, :, :, [0]]
+        if self.mode == 'mono':
+            x_test = x_test[:, :, :, [0]]
+        elif self.mode == 'color':
+            pass
         decoded_imgs = self.autoencoder.predict(x_test)
+
 
         plt.figure(figsize=(16, 4))
         for i in range(num_images):
             # Original
             ax = plt.subplot(2, num_images, i + 1)
-            plt.imshow(x_test[i].reshape(28, 28))
-            plt.gray()
+            if self.mode == 'mono':
+                plt.imshow(x_test[i].reshape(28, 28), cmap='gray')
+            elif self.mode == 'color':
+                plt.imshow(x_test[i].reshape(28, 28, 3)*255)
             ax.set_title('Original')
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
 
             # Reconstruction
             ax = plt.subplot(2, num_images, i + 1 + num_images)
-            plt.imshow(decoded_imgs[i].reshape(28, 28))
-            plt.gray()
+            if self.mode == 'mono':
+                plt.imshow(decoded_imgs[i].reshape(28, 28), cmap='gray')
+            elif self.mode == 'color':
+                plt.imshow(decoded_imgs[i].reshape(28, 28, 3))
             ax.set_title('Reconstructed')
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
@@ -129,8 +145,10 @@ class AutoEncoderNet:
         plt.suptitle('Randomly generated images')
         for i in range(num_images):
             ax = plt.subplot(1, num_images, i + 1)
-            plt.imshow(generated_images[i].numpy().reshape(28, 28))
-            plt.gray()
+            if self.mode == 'mono':
+                plt.imshow(generated_images[i].numpy().reshape(28, 28), cmap='gray')
+            elif self.mode == 'color':
+                plt.imshow(generated_images[i].numpy().reshape(28, 28, 3))
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
         plt.show()
@@ -147,34 +165,43 @@ class AutoEncoderNet:
         """
         Display images with the highest reconstruction error
         """
-        x_test, _ = generator.get_full_data_set(training=False)
-        x_test = x_test[:, :, :, [0]]
+        x_test, y_test = generator.get_full_data_set(training=False)
+        if self.mode == 'mono':
+            x_test = x_test[:, :, :, [0]]
+        elif self.mode == 'color':
+            pass
         errors = self.calculate_reconstruction_error(x_test)
         
         # Find the images with the highest reconstruction error
         idxs = np.argsort(errors)[-num_images:]
         anomalous_images = x_test[idxs]
+        anomalous_labels = y_test[idxs]
         
         plt.figure(figsize=(16, 4))
         plt.suptitle('Images with the highest reconstruction error')
         for i, idx in enumerate(idxs):
             ax = plt.subplot(1, num_images, i + 1)
-            plt.imshow(anomalous_images[i].reshape(28, 28))
-            plt.gray()
+            if self.mode == 'mono':
+                plt.imshow(anomalous_images[i].reshape(28, 28), cmap='gray')
+                label = str(anomalous_labels[i])[-1]
+                ax.set_title(f"{label}")
+            elif self.mode == 'color':
+                plt.imshow(anomalous_images[i].reshape(28, 28, 3)*255)
+                ax.set_title(f"{anomalous_labels[i]}")
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
         plt.show()
 
 if __name__ == "__main__":
-    gen = StackedMNISTData(mode=DataMode.MONO_BINARY_COMPLETE, default_batch_size=2048)
-    net = AutoEncoderNet(encoding_dim=128, file_name="./models/autoencoder/autoencoder")
+    gen = StackedMNISTData(mode=DataMode.COLOR_BINARY_COMPLETE, default_batch_size=2048)
+    net = AutoEncoderNet(mode='mono', encoding_dim=128, file_name="./models/autoencoder/autoencoder_missing")
     net.train(generator=gen, epochs=10)
 
     # Reconstruct some images
-    net.show_reconstructions(generator=gen, num_images=10)
+    # net.show_reconstructions(generator=gen, num_images=10)
 
     # Generate new images
-    net.show_generated_images(num_images=10)
+    # net.show_generated_images(num_images=10)
 
     # Display images with the highest reconstruction error
     net.display_anomalies(generator=gen, num_images=10)
@@ -202,5 +229,22 @@ Anomaly Detection
 -----------------
 The auto encoder can detect anomalies in the dataset by calculating the reconstruction error for each image. The images with
 the highest reconstruction error are considered anomalies. The auto encoder can detect anomalies with high accuracy.
+
+
+--------
+Comments
+--------
+To make the model support colors, I added the 'mode' parameter to the AutoEncoderNet class. The 'mode' parameter can be 'mono'
+for one-channel images or 'color' for three-channel images. The code uses this parameter to decide how to treat the data in
+other parts of the code.
+
+Pre-trained models include:
+'./models/autoencoder/autoencoder' for one-channel images (complete)
+'./models/autoencoder/autoencoder_missing' for one-channel images (incomplete)
+'./models/autoencoder/autoencoder_color' for three-channel images (complete)
+'./models/autoencoder/autoencoder_color_missing' for three-channel images (incomplete)
+
+It's a red flag the the auto encoder trained on a missing value gives the same anomalies as the complete auto encoder. Figure
+out which number is actually missing, and check if the auto encoder can detect it.
 
 """
